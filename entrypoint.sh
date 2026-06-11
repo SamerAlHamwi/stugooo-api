e9ce231c0132538138f37bb8be32d88abebb1131
rafahdakakni <rafah.dakakni@gmail.com>
2026-06-11 23:51:25 +0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 php artisan storage:link --force 2>/dev/null || true
 
 php artisan migrate --force
